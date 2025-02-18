c4cf562d7aeb1da010ad47e26ade15d3830b3648
craneppeakare <25124648+craneppeakare@users.noreply.github.com>
2025-02-17 17:44:08 -0800

    fix workflow pathing

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/config.ts',
+  entry: "./src/config.ts",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        include: path.resolve(__dirname, 'src'),
-        loader: 'ts-loader'
+        include: path.resolve(__dirname, "src"),
+        loader: "ts-loader",
       },
       {
-        test: require.resolve('phaser'),
-        loader: 'expose-loader',
-        options: { exposes: { globalName: 'Phaser', override: true } }
-      }
-    ]
+        test: require.resolve("phaser"),
+        loader: "expose-loader",
+        options: { exposes: { globalName: "Phaser", override: true } },
+      },
+    ],
   },
   devServer: {
-    static: path.resolve(__dirname, './'),
-    host: 'localhost',
+    static: path.resolve(__dirname, "./"),
+    host: "localhost",
     port: 8080,
-    open: false
+    open: false,
   },
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: [".ts", ".js"],
+  },
 };
