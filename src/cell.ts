2c38ecfe6c25afccd93f8dc6b6a6a6feaf0d7ca1
craneppeakare <25124648+craneppeakare@users.noreply.github.com>
2025-02-17 17:17:50 -0800

    update game

--- src/cell.ts
+++ src/cell.ts
@@ -1,234 +1,262 @@
-import * as Phaser from 'phaser';
-import * as Config from './config';
-import Chunk from './chunk';
+import * as Phaser from "phaser";
+import * as Config from "./config";
+import Chunk from "./chunk";
 
 export default class Cell extends Phaser.GameObjects.Rectangle {
-    static TILE_SIZE = 64;
-    private static COVER_EVEN_COLOR = 0x37aa56;
-    private static COVER_ODD_COLOR = 0x268d41;
-    private static REVEALED_EVEN_COLOR = 0xcfbb93;
-    private static REVEALED_ODD_COLOR = 0xba9872;
-    private static LABEL_COLORS = ['#0000ff',
-                                  '#008000',
-                                  '#ff0000',
-                                  '#000080',
-                                  '#800000',
-                                  '#008080',
-                                  '#808080'];
-    private static TILE_POINTS = [0,
-                                 10,
-                                 80,
-                                 270,
-                                 640,
-                                 1250,
-                                 2160,
-                                 3430,
-                                 5120]
+  static TILE_SIZE = 64;
+  private static COVER_EVEN_COLOR = 0x37aa56;
+  private static COVER_ODD_COLOR = 0x268d41;
+  private static REVEALED_EVEN_COLOR = 0xcfbb93;
+  private static REVEALED_ODD_COLOR = 0xba9872;
+  private static LABEL_COLORS = [
+    "#0000ff",
+    "#008000",
+    "#ff0000",
+    "#000080",
+    "#800000",
+    "#008080",
+    "#808080",
+  ];
+  private static TILE_POINTS = [0, 10, 80, 270, 640, 1250, 2160, 3430, 5120];
 
-    private id: {x: number, y: number, chunkId: number};
-    private labelStyle = { fontFamily: 'Silkscreen', fontSize: '28px', stroke: '#000000', strokeThickness: 0 };
-    private flagImage: Phaser.GameObjects.Image;
-    private mineImage: Phaser.GameObjects.Image;
-    private clickHighlight: Phaser.GameObjects.Rectangle;
-    private bevelPolygons: Phaser.GameObjects.Polygon[];
-    private digParticleEmitter: Phaser.GameObjects.Particles.ParticleEmitter;
+  private id: { x: number; y: number; chunkId: number };
+  private labelStyle = {
+    fontFamily: "Silkscreen",
+    fontSize: "28px",
+    stroke: "#000000",
+    strokeThickness: 0,
+  };
+  private flagImage: Phaser.GameObjects.Image;
+  private mineImage: Phaser.GameObjects.Image;
+  private clickHighlight: Phaser.GameObjects.Rectangle;
+  private bevelPolygons: Phaser.GameObjects.Polygon[];
+  private digParticleEmitter: Phaser.GameObjects.Particles.ParticleEmitter;
 
-    chunk: Chunk;
-    isAMine = false;
-    isRevealed = false;
-    isFlagged = false;
-    minesNearby = 0;
-    label;
+  chunk: Chunk;
+  isAMine = false;
+  isRevealed = false;
+  isFlagged = false;
+  minesNearby = 0;
+  label;
 
-    /**
-    * Constructor for a Cell
-    * 
-    * @param scene - The scene which this Cell belongs to
-    * @param pos - The position of the Cell on the screen
-    * @param id - The id of the Cell
-    * @param chunk - The chunk which this cell belongs to
-    *
-    * @returns a Cell
-    */
-    constructor(scene: Phaser.Scene, pos: { x: number, y: number }, id: { x: number, y: number, chunkId: number}, chunk: Chunk) {
-        super(scene, pos.x, pos.y, 64, 64, 0xffffff);
-        this.setOrigin(0, 0)
-            .setInteractive()
-            .on('pointerup', this.on_pointerup);
+  /**
+   * Constructor for a Cell
+   *
+   * @param scene - The scene which this Cell belongs to
+   * @param pos - The position of the Cell on the screen
+   * @param id - The id of the Cell
+   * @param chunk - The chunk which this cell belongs to
+   *
+   * @returns a Cell
+   */
+  constructor(
+    scene: Phaser.Scene,
+    pos: { x: number; y: number },
+    id: { x: number; y: number; chunkId: number },
+    chunk: Chunk,
+  ) {
+    super(scene, pos.x, pos.y, 64, 64, 0xffffff);
+    this.setOrigin(0, 0).setInteractive().on("pointerup", this.on_pointerup);
 
-        this.id = id;
-        this.chunk = chunk;
-        scene.add.existing(this);
-        this.label = this.scene.add.text(this.getCenter().x, this.getCenter().y, '', Config.defaultStyle)
-            .setFontSize(28)
-            .setOrigin(0.5, 0.5);
-        this.clickHighlight = this.scene.add.rectangle(pos.x, pos.y, 64, 64, 0xffffff, 150)
-            .setOrigin(0, 0)
-            .setVisible(false);
-        this.flagImage = this.scene.add.image(this.getCenter().x, this.getCenter().y, 'flagImage')
-            .setVisible(false);
-        this.updateColor();
-        this.digParticleEmitter = this.scene.add.particles(0, 0, 'digParticle', {
-            emitZone: new Phaser.GameObjects.Particles.Zones.RandomZone(this.geom),
-            alpha: { start: 1, end: 0 },
-            rotate: { min: 0, max: 360 },
-            quantity: { min: 8, max: 15 },
-            gravityY: 1300,
-            scale: 0.5,
-            speed: { min: 100, max: 250 },
-            lifespan: 800,
-            emitting: false,
+    this.id = id;
+    this.chunk = chunk;
+    scene.add.existing(this);
+    this.label = this.scene.add
+      .text(this.getCenter().x, this.getCenter().y, "", Config.defaultStyle)
+      .setFontSize(28)
+      .setOrigin(0.5, 0.5);
+    this.clickHighlight = this.scene.add
+      .rectangle(pos.x, pos.y, 64, 64, 0xffffff, 150)
+      .setOrigin(0, 0)
+      .setVisible(false);
+    this.flagImage = this.scene.add
+      .image(this.getCenter().x, this.getCenter().y, "flagImage")
+      .setVisible(false);
+    this.updateColor();
+    this.digParticleEmitter = this.scene.add.particles(0, 0, "digParticle", {
+      emitZone: new Phaser.GameObjects.Particles.Zones.RandomZone(this.geom),
+      alpha: { start: 1, end: 0 },
+      rotate: { min: 0, max: 360 },
+      quantity: { min: 8, max: 15 },
+      gravityY: 1300,
+      scale: 0.5,
+      speed: { min: 100, max: 250 },
+      lifespan: 800,
+      emitting: false,
+    });
+    this.digParticleEmitter.depth = 1;
+
+    this.on("pointerdown", () => {
+      this.clickHighlight.setVisible(true);
+      if (this.isRevealed && this.minesNearby) {
+        chunk.getNeighborTiles(id.x, id.y).forEach((cell) => {
+          cell.clickHighlight.setVisible(true);
         });
-        this.digParticleEmitter.depth = 1;
-
-        this.on('pointerdown', () => {
-            this.clickHighlight.setVisible(true);
-            if (this.isRevealed && this.minesNearby) {
-                chunk.getNeighborTiles(id.x, id.y).forEach(cell => {
-                    cell.clickHighlight.setVisible(true);
-                });
-            }
+      }
+    });
+    const disableHighlight = () => {
+      this.clickHighlight.setVisible(false);
+      if (this.isRevealed && this.minesNearby) {
+        chunk.getNeighborTiles(id.x, id.y).forEach((cell) => {
+          cell.clickHighlight.setVisible(false);
         });
-        const disableHighlight = () => {
-            this.clickHighlight.setVisible(false);
-            if (this.isRevealed && this.minesNearby) {
-                chunk.getNeighborTiles(id.x, id.y).forEach(cell => {
-                    cell.clickHighlight.setVisible(false);
-                });
-            }
-        }
-        this.on('pointerout', disableHighlight);
-        this.on('pointerup', disableHighlight);
-    }
+      }
+    };
+    this.on("pointerout", disableHighlight);
+    this.on("pointerup", disableHighlight);
+  }
 
-    /**
-    * Reveals the tile
-    *
-    * @returns number for amount of points
-    */
-    reveal(): number {
-        if (this.isFlagged || this.isRevealed) return 0;
-        this.isRevealed = true;
-        this.clickHighlight.setVisible(false);
-        this.updateColor();
-        if (this.isAMine) {
-            this.mineImage = this.scene.add.image(this.getCenter().x, this.getCenter().y, 'mineImage');
-            this.scene.events.emit('gameover');
-            return 0;
-        } else {
-            this.digParticleEmitter.emitParticleAt(this.getTopLeft().x, this.getTopLeft().y);
-            if (this.minesNearby) {
-                this.label.setText(this.minesNearby.toString());
-                this.label.setColor(Cell.LABEL_COLORS[this.minesNearby - 1]);
-            }
-            return Cell.TILE_POINTS[this.minesNearby];
-        }
+  /**
+   * Reveals the tile
+   *
+   * @returns number for amount of points
+   */
+  reveal(): number {
+    if (this.isFlagged || this.isRevealed) return 0;
+    this.isRevealed = true;
+    this.clickHighlight.setVisible(false);
+    this.updateColor();
+    if (this.isAMine) {
+      this.mineImage = this.scene.add.image(
+        this.getCenter().x,
+        this.getCenter().y,
+        "mineImage",
+      );
+      this.scene.events.emit("gameover");
+      return 0;
+    } else {
+      this.digParticleEmitter.emitParticleAt(
+        this.getTopLeft().x,
+        this.getTopLeft().y,
+      );
+      if (this.minesNearby) {
+        this.label.setText(this.minesNearby.toString());
+        this.label.setColor(Cell.LABEL_COLORS[this.minesNearby - 1]);
+      }
+      return Cell.TILE_POINTS[this.minesNearby];
     }
+  }
 
-    /**
-    * Places a flag on top of the tile and prevents revealing
-    *
-    * @returns void
-    */
-    flag() {
-        this.isFlagged = !this.isFlagged;
-        if (this.isFlagged) {
-            this.flagImage.setVisible(true);
-        } else {
-            this.flagImage.setVisible(false);
-        }
+  /**
+   * Places a flag on top of the tile and prevents revealing
+   *
+   * @returns void
+   */
+  flag() {
+    this.isFlagged = !this.isFlagged;
+    if (this.isFlagged) {
+      this.flagImage.setVisible(true);
+    } else {
+      this.flagImage.setVisible(false);
     }
+  }
 
-    /**
-    * @returns the id of this Cell
-    */
-    getId(): {x: number, y: number, chunkId: number} {
-        return Object.create(this.id);
+  /**
+   * @returns the id of this Cell
+   */
+  getId(): { x: number; y: number; chunkId: number } {
+    return Object.create(this.id);
+  }
+
+  /**
+   * Increments this.minesNearby and updates the label
+   *
+   * @returns void
+   */
+  incrementMinesNearby() {
+    this.minesNearby += 1;
+    if (this.isRevealed) {
+      this.label.setText(this.minesNearby.toString());
+      this.label.setColor(Cell.LABEL_COLORS[this.minesNearby - 1]);
     }
+  }
 
+  /**
+   * Scrolls the cell down the screen
+   *
+   * @param by - distance in units to move the Cell by
+   *
+   * @returns void
+   */
+  scrollCellDown(dy: number) {
+    this.scene.tweens.add({
+      targets: [
+        this,
+        this.label,
+        this.flagImage,
+        this.bevelPolygons,
+        this.clickHighlight,
+      ].flat(),
+      y: "+=" + dy,
+      ease: "Linear",
+      onStart: () => this.disableInteractive(),
+      onComplete: () => this.setInteractive(),
+      duration: 1225,
+    });
+  }
 
-    /**
-    * Increments this.minesNearby and updates the label
-    *
-    * @returns void
-    */
-    incrementMinesNearby() {
-        this.minesNearby += 1;
-        if (this.isRevealed) {
-            this.label.setText(this.minesNearby.toString());
-            this.label.setColor(Cell.LABEL_COLORS[this.minesNearby - 1]);
-        }
+  /**
+   * Destroy all game elements of the Cell allowing it to be garbage collected
+   *
+   * @returns void
+   */
+  destroyAll() {
+    this.flagImage.destroy();
+    if (this.mineImage) this.mineImage.destroy();
+    this.clickHighlight.destroy();
+    this.bevelPolygons.forEach((p) => p.destroy());
+    this.label.destroy();
+    this.digParticleEmitter.destroy();
+    this.destroy();
+  }
+
+  /**
+   * Emits an event to communicate to the top level scene that this cell has been clicked on
+   *
+   * @returns void
+   */
+  private on_pointerup() {
+    this.scene.events.emit("tile-pressed", this.id);
+  }
+
+  /**
+   * Updates the color that the tile should be in depending on if it's revealed or not.
+   *
+   * @returns void
+   */
+  private updateColor() {
+    const isEvenCell = (this.id.x + this.id.y) % 2;
+    if (this.isRevealed) {
+      this.bevelPolygons.forEach((p) => p.destroy());
+      this.bevelPolygons = [];
+      if (this.isAMine) this.fillColor = isEvenCell ? 0xaa0000 : 0x880000;
+      else
+        this.fillColor = isEvenCell
+          ? Cell.REVEALED_EVEN_COLOR
+          : Cell.REVEALED_ODD_COLOR;
+    } else {
+      this.fillColor = isEvenCell
+        ? Cell.COVER_EVEN_COLOR
+        : Cell.COVER_ODD_COLOR;
+      const x = this.getTopLeft().x;
+      const y = this.getTopLeft().y;
+      isEvenCell
+        ? (this.bevelPolygons = [
+            this.scene.add
+              .polygon(x, y, "0 0 64 0 60 4 4 4 4 60 0 64", 0x4ec16d)
+              .setOrigin(0, 0),
+            this.scene.add
+              .polygon(x, y, "0 64 4 60 60 60 60 4 64 0 64 64", 0x3a814d)
+              .setOrigin(0, 0),
+          ])
+        : (this.bevelPolygons = [
+            this.scene.add
+              .polygon(x, y, "0 0 64 0 60 4 4 4 4 60 0 64", 0x41b65b)
+              .setOrigin(0, 0),
+            this.scene.add
+              .polygon(x, y, "0 64 4 60 60 60 60 4 64 0 64 64", 0x2a7a3f)
+              .setOrigin(0, 0),
+          ]);
     }
-
-    /**
-    * Scrolls the cell down the screen
-    *
-    * @param by - distance in units to move the Cell by
-    *
-    * @returns void
-    */
-    scrollCellDown(dy: number) {
-        this.scene.tweens.add({
-            targets: [this, this.label, this.flagImage, this.bevelPolygons, this.clickHighlight].flat(),
-            y: '+=' + dy,
-            ease: 'Linear',
-            onStart: () => this.disableInteractive(),
-            onComplete: () => this.setInteractive(),
-            duration: 1225,
-        });
-    }
-
-    /**
-    * Destroy all game elements of the Cell allowing it to be garbage collected
-    *
-    * @returns void
-    */
-    destroyAll() {
-        this.flagImage.destroy();
-        if (this.mineImage) this.mineImage.destroy();
-        this.clickHighlight.destroy();
-        this.bevelPolygons.forEach(p => p.destroy());
-        this.label.destroy();
-        this.digParticleEmitter.destroy();
-        this.destroy();
-    }
-
-    /**
-    * Emits an event to communicate to the top level scene that this cell has been clicked on
-    *
-    * @returns void
-    */
-    private on_pointerup() {
-        this.scene.events.emit('tile-pressed', this.id);
-    }
-
-    /**
-     * Updates the color that the tile should be in depending on if it's revealed or not.
-     *
-     * @returns void
-     */
-    private updateColor() {
-        const isEvenCell = (this.id.x+this.id.y) % 2;
-        if (this.isRevealed) {
-            this.bevelPolygons.forEach(p => p.destroy());
-            this.bevelPolygons = [];
-            if (this.isAMine)
-                this.fillColor = isEvenCell ? 0xaa0000 : 0x880000;
-            else
-                this.fillColor = isEvenCell ? Cell.REVEALED_EVEN_COLOR : Cell.REVEALED_ODD_COLOR;
-        } else {
-            this.fillColor = isEvenCell ? Cell.COVER_EVEN_COLOR : Cell.COVER_ODD_COLOR;
-            const x = this.getTopLeft().x;
-            const y = this.getTopLeft().y;
-            isEvenCell
-                ? this.bevelPolygons = [
-                    this.scene.add.polygon(x, y, "0 0 64 0 60 4 4 4 4 60 0 64", 0x4ec16d).setOrigin(0, 0),
-                    this.scene.add.polygon(x, y, "0 64 4 60 60 60 60 4 64 0 64 64", 0x3a814d).setOrigin(0, 0),
-                    ]
-                : this.bevelPolygons = [
-                    this.scene.add.polygon(x, y, "0 0 64 0 60 4 4 4 4 60 0 64", 0x41b65b).setOrigin(0, 0),
-                    this.scene.add.polygon(x, y, "0 64 4 60 60 60 60 4 64 0 64 64", 0x2a7a3f).setOrigin(0, 0),
-                    ];
-        }
-    }
+  }
 }
--- src/gameover.ts
+++ src/gameover.ts
@@ -1,75 +1,83 @@
-import * as Phaser from 'phaser';
-import * as Config from './config';
-import Button from './button';
+import * as Phaser from "phaser";
+import * as Config from "./config";
+import Button from "./button";
 
 export default class GameOver extends Phaser.Scene {
-    private score: number;
-    private highscore: number;
-    private maxDistance: number;
+  private score: number;
+  private highscore: number;
+  private maxDistance: number;
 
-    /**
-    * Constructor for the main Scene
-    *
-    * @returns a Scene
-    */
-    constructor () {
-        super('Gameover');
-    }
+  /**
+   * Constructor for the main Scene
+   *
+   * @returns a Scene
+   */
+  constructor() {
+    super("Gameover");
+  }
 
-    /**
-    * Initialize method. Runs before preload() and create().
-    *
-    * @returns void
-    */
-    init (data: any) {
-        this.score = data.score;
-        this.highscore = data.highscore;
-        this.maxDistance = data.maxDistance;
-    }
+  /**
+   * Initialize method. Runs before preload() and create().
+   *
+   * @returns void
+   */
+  init(data: any) {
+    this.score = data.score;
+    this.highscore = data.highscore;
+    this.maxDistance = data.maxDistance;
+  }
 
-    /**
-    * Preloads any assets needed for the scene. Runs before the create() method.
-    *
-    * @returns void
-    */
-    preload () {
-    }
+  /**
+   * Preloads any assets needed for the scene. Runs before the create() method.
+   *
+   * @returns void
+   */
+  preload() { }
 
-    /**
-    * Runs once when scene is created. Spawns the necessary chunks for the game.
-    *
-    * @returns void
-    */
-    create () {
-        this.sound.stopByKey('bg');
-        window.navigator.vibrate(200);
+  /**
+   * Runs once when scene is created. Spawns the necessary chunks for the game.
+   *
+   * @returns void
+   */
+  create() {
+    this.sound.stopByKey("bg");
+    this.cameras.main.setBackgroundColor("rgba(0, 0, 0, 150)");
 
-        const gameoverString = `GAME OVER\n\nDISTANCE: ${this.maxDistance}m\nSCORE: ${this.score}\nHIGHSCORE: ${this.highscore}`;
-        // const gameoverString = `I LOVE YOU CJ\n\nDISTANCE: ${this.maxDistance}m\nSCORE: ${this.score}\nHIGHSCORE: ${this.highscore}`;
-        this.add.text(Config.GAME_WIDTH/2, Config.GAME_HEIGHT/2, gameoverString)
-            .setStyle({ ...Config.largeStyle, align: 'center' })
-            .setOrigin(0.5, 0.5);
+    const gameoverString = `GAME OVER\n\nDISTANCE: ${this.maxDistance}m\nSCORE: ${this.score}\nHIGHSCORE: ${this.highscore}`;
+    this.add
+      .text(Config.GAME_WIDTH / 2, Config.GAME_HEIGHT / 2, gameoverString)
+      .setStyle({ ...Config.largeStyle, align: "center" })
+      .setOrigin(0.5, 0.5);
 
-        this.game.scene.pause("Infinite Minesweeper");
-        this.game.scene.pause("Overlay");
+    this.game.scene.pause("Infinite Minesweeper");
+    this.game.scene.pause("Overlay");
 
-        this.scene.moveUp();
+    this.scene.moveUp();
 
-        const restartButton = new Button(this, Config.GAME_WIDTH/2, 800, "Restart");
-        restartButton.onClick(() => {
-            if (Config.GameSettings.playBGM) {
-                // this.sound.stopByKey('bg');
-                this.sound.play('bg', { loop: true });
-            }
-            this.scene.start("Infinite Minesweeper");
-            this.scene.start("Overlay");
-        });
+    const restartButton = new Button(
+      this,
+      Config.GAME_WIDTH / 2,
+      800,
+      "Restart",
+    );
+    restartButton.onClick(() => {
+      if (Config.GameSettings.playBGM) {
+        this.sound.play("bg", { loop: true });
+      }
+      this.scene.start("Infinite Minesweeper");
+      this.scene.start("Overlay");
+    });
 
-        const backToMenuButton = new Button(this, Config.GAME_WIDTH/2, 900, "Back To Menu");
-        backToMenuButton.onClick(() => {
-            this.game.scene.stop("Infinite Minesweeper");
-            this.game.scene.stop("Overlay");
-            this.scene.start("MainMenu");
-        });
-    }
+    const backToMenuButton = new Button(
+      this,
+      Config.GAME_WIDTH / 2,
+      900,
+      "Back To Menu",
+    );
+    backToMenuButton.onClick(() => {
+      this.game.scene.stop("Infinite Minesweeper");
+      this.game.scene.stop("Overlay");
+      this.scene.start("MainMenu");
+    });
+  }
 }
